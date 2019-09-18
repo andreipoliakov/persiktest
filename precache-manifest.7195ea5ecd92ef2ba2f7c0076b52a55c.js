84a676cdf806defb5ea5109f5aef439bb3f5c821
Andrei Poliakov <k_rakozabrik@mail.ru>
2019-09-18 23:36:48 +0300

    Updates

---
+++ precache-manifest.7195ea5ecd92ef2ba2f7c0076b52a55c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.8c97409f.js"
+  },
+  {
+    "revision": "a3e5e72355e1b5e6b12a",
+    "url": "./static/js/main.a3e5e723.chunk.js"
+  },
+  {
+    "revision": "185d602024bed90731a2",
+    "url": "./static/js/2.185d6020.chunk.js"
+  },
+  {
+    "revision": "a3e5e72355e1b5e6b12a",
+    "url": "./static/css/main.860f36d0.chunk.css"
+  },
+  {
+    "revision": "185d602024bed90731a2",
+    "url": "./static/css/2.5226c3ef.chunk.css"
+  },
+  {
+    "revision": "4fe6b43a3c9bb8c2bb29a97dfeebe76a",
+    "url": "./index.html"
+  }
+];
